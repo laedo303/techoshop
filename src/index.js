d2e49f5e60d90946518f2cb5ad52f0540ba4d54c
Konstantin <laedo303@mail.ru>
2022-07-12 02:14:19 +0500

    add-styles

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import './index.scss';
 import Swiper, {Thumbs, Scrollbar, Navigation, Pagination} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
 
 const thumbSwiper = new Swiper('.card__slider-thumb', {
   spaceBetween: 44,
@@ -32,6 +35,22 @@ new Swiper('.card__image', {
 
 new Swiper('.recommended__carousel', {
   spaceBetween: 30,
+  slidesPerView: 5,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+   modules: [Navigation, Pagination]
+});
+
+new Swiper('.catalog__pagination', {
+  spaceBetween: 36,
   slidesPerView: 5,
 
   navigation: {
